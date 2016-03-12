b949cd1330084def6f877754c027e31392a8769b
Denis Minier <denis.minier@gmail.com>
2016-03-12 17:29:55 +0100

    Release 3.0

--- app/js/app.js
+++ app/js/app.js
@@ -2369,16 +2369,16 @@ Date.prototype.getWeek = function() {
     // 60*60*24*1000 = 86400000
     // 'onejan_next_monday_time' reffers to the miliseconds of the next monday after 01/01
 
-    var day_miliseconds = 86400000,
-        onejan = new Date(this.getFullYear(),0,1,0,0,0),
-        onejan_day = (onejan.getDay() ===0) ? 7 : onejan.getDay(),
-        days_for_next_monday = (8-onejan_day),
-        onejan_next_monday_time = onejan.getTime() + (days_for_next_monday * day_miliseconds),
+    var day_miliseconds = 86400000;
+    var onejan = new Date(this.getFullYear(),0,1,0,0,0);   
+    var onejan_day = (onejan.getDay() ===0) ? 7 : onejan.getDay();
+    var days_for_next_monday = (8-onejan_day);  
+    var onejan_next_monday_time = onejan.getTime() + (days_for_next_monday * day_miliseconds);
         // If one jan is not a monday, get the first monday of the year
-        first_monday_year_time = (onejan_day>1) ? onejan_next_monday_time : onejan.getTime(),
-        this_date = new Date(this.getFullYear(), this.getMonth(),this.getDate(),0,0,0),// This at 00:00:00
-        this_time = this_date.getTime(),
-        days_from_first_monday = Math.round(((this_time - first_monday_year_time) / day_miliseconds));
+    var first_monday_year_time = (onejan_day>1) ? onejan_next_monday_time : onejan.getTime();
+    var this_date = new Date(this.getFullYear(), this.getMonth(),this.getDate(),0,0,0); // This at 00:00:00
+    var this_time = this_date.getTime(); 
+    var days_from_first_monday = Math.round(((this_time - first_monday_year_time) / day_miliseconds));
 
     var first_monday_year = new Date(first_monday_year_time);
 
